be710cd3c587777151f318a5a655e872e2bd3318
teskew <teskinder1@gmail.com>
2020-11-15 19:29:35 -0600

    commit tree 86c443aaa81599a2691bbcd57255efbf0db6183c

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
+
 def create_an_empty_array
-  
+a = []
+      
+      
 end
 
 def create_an_array
-  
+  a = ["w","x","y","z"]
 end
 
 def add_element_to_end_of_array(array, element)
+  array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  x = array.count
+  array[x-1]
 end
